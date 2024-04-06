b1caed0cfa55e217828ca49ce20fb7c7bd811007
Guilherme Henrique <guihchavesb@gmail.com>
2024-04-06 00:13:28 +0000

    check network

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+if docker network ls | grep 'traefik_server_apps'; then
+    echo 'Network found'
+else
+    echo 'Network not found creating...'
+    docker network create traefik_server_apps
+fi
+
 docker compose up -d traefik
